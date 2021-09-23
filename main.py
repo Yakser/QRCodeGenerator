2565b917b05614769c34909cf11125136f9dcf41
Sergey Yaksanov <iaksanov2044@mail.ru>
2021-09-23 20:54:50 +0400

    deploy

--- main.py
+++
@@ -1,5 +0,0 @@
-import uvicorn
-
-
-if __name__ == "__main__":
-    uvicorn.run("src.api.app:app", host="0.0.0.0", port=5000)
--- src/api/app.py
+++ src/api/app.py
@@ -21,10 +21,6 @@ FILE_PATH = 'static/qr-code-'
 
 # from tokens_storage import TokensStorage
 
-
-FILE_PATH = 'static/qr-code-'
-
-
 class TokensStorage:
     # todo make self.__tokens dict
     def __init__(self):
@@ -115,15 +111,12 @@ app = FastAPI()
 # project_root_absolute = project_root.resolve()
 # static_root_absolute = project_root_absolute
 
-app.mount("/static", StaticFiles(directory='./src/api/static'), name="static")
+app.mount("/static", StaticFiles(directory='./static'), name="static")
 storage = TokensStorage()
  
-
-app = FastAPI()
-
 origins = [
-    "http://localhost:3000",
-    "localhost:3000"
+    "http://0.0.0.0:3000",
+    "0.0.0.0:3000"
 ]
 
 
@@ -150,5 +143,5 @@ async def generate_page(background_tasks: BackgroundTasks, text: str):
         return {'message': 'error'}
 
 
-# if __name__ == '__main__':
-#    uvicorn.run('app:app', host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+   uvicorn.run('app:app', host="0.0.0.0", port=5000)
